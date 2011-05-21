0477ee74eda9c380f26ccc4289204d12fbef4e9d
Zdenek Styblik <stybla@turnovfree.net>
2011-05-21 09:00:52 +0200

    MOD pkg descriptions, polished code a lot Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Signed-off-by: Zdeněk Styblík <stybla@turnovfree.net>

--- maketags.sh
+++ maketags.sh
@@ -13,8 +13,10 @@
 # Ideas: 
 # - there could be menu A-Y like during an installation
 #
+set -u
 # Where can we find mounted Slackware CD/DVD/etc.
 SLACKDIR='/mnt/cdrom/'
+#TMP='/tmp/'
 # Read package description from TXT files
 # WARNING: it doesn't work, it's unsupported, don't use !!!
 PKGDESC=1
@@ -32,7 +34,7 @@ PKGDESC=1
 #read UINPUT
 #if [ ! -z ./SLACKDIR ]; then
 #	SLACKDIR=$UINPUT
-#fi;
+#fi
 #################################
 
 rm -f ./SLACKDIR 2>/dev/null
@@ -43,54 +45,59 @@ dialog --title "Slackware directory" --inputbox "Please, enter path \
 to Slackware directory \
 eg. mounted CD/DVD-ROM, or use default (${SLACKDIR})" 8 70 2>SLACKDIR
 if [ -s './SLACKDIR' ]; then
-	SLACKDIR=`cat ./SLACKDIR`
+	SLACKDIR=$(cat ./SLACKDIR)
 	rm -f ./SLACKDIR
-fi;
+fi
 
-LSOUT=`ls /${SLACKDIR}/ 2>/dev/null | egrep '^slackware(64)?$'`
-if [ $? -ne 0 ]; then 
+if [ ! -d "${SLACKDIR}/slackware" ] && [ ! -d "${SLACKDIR}/slackware64" ]; then 
 	echo "There seems to be no slackware directory in ${SLACKDIR}"
 	echo "Make sure Slackware is there, or check the permissions"
 	exit 1
-fi;
+fi
 
+LSOUT=$(ls "${SLACKDIR}" 2>/dev/null | grep -E -e '^slackware(64)?$')
 # there might be more than one dir, but it's unlikely.
-PKGDIR=`echo ${LSOUT} | cut -d ' ' -f 1`
+PKGDIR=$(echo ${LSOUT} | cut -d ' ' -f 1)
 
 REUSETAGS=0
-if [ -e ./${PKGDIR} ]; then
+if [ -d "./${PKGDIR}" ]; then
 	dialog --title "Old tag files" --yesno "It looks I've found the \
 last set of tag files you've generated. Should I re-use them, if \
 possible, or start over (= delete)?" 10 72
 	if [ $? == 0 ]; then
 		REUSETAGS=1
-		rm -Rf ./${PKGDIR}.old
-		mv ./${PKGDIR} ./${PKGDIR}.old
+		rm -Rf "./${PKGDIR}.old"
+		mv "./${PKGDIR}" "./${PKGDIR}.old"
 	else
 		REUSETAGS=0
-		rm -Rf ./${PKGDIR}
-	fi;
-fi;
+		rm -Rf "./${PKGDIR}"
+	fi
+fi
 
-mkdir ./${PKGDIR}
-
-if [ $? -ne 0 ]; then
-	echo "Unable to create directory ${PKGDIR}/"
+if ! $(mkdir "./${PKGDIR}" 2>/dev/null) ; then
+	echo "Unable to create directory './${PKGDIR}'"
 	exit 1
-fi;
+fi
 
-SLACKVER=`head /${SLACKDIR}/README.TXT | grep Welcome | \
-sed 's/Welcome to //' | sed 's/!//'`
-DATE=`date`
-cat /dev/null > ./${PKGDIR}/tagfiles.nfo
-echo "# Slackware PKG TAG file" >> ./${PKGDIR}/tagfiles.nfo
-echo "# Generated on `date` " >> ./${PKGDIR}/tagfiles.nfo
-echo "# for " >> ./${PKGDIR}/tagfiles.nfo
-echo "# ${SLACKVER}" >> ./${PKGDIR}/tagfiles.nfo
+SLACKVER=$(head "/${SLACKDIR}/README.TXT" | grep -e 'Welcome' | \
+	sed -e 's/Welcome to //' | sed -e 's/!//')
+DATE=$(date)
+cat /dev/null > "./${PKGDIR}/tagfiles.nfo"
+echo "# Slackware PKG TAG file" >> "./${PKGDIR}/tagfiles.nfo"
+echo "# Generated on ${DATE}" >> "./${PKGDIR}/tagfiles.nfo"
+echo "# for " >> "./${PKGDIR}/tagfiles.nfo"
+echo "# ${SLACKVER}" >> "./${PKGDIR}/tagfiles.nfo"
 
-for CAT in `ls -gG1 /${SLACKDIR}/${PKGDIR}/ | grep ^d |\
- 	awk '{ print $6 }'`; do 
+if [ ! -e "/${SLACKDIR}/CHECKSUMS.md5" ]; then
+	echo "File '${SLACKDIR}/CHECKSUMS.md5' doesn't seem to exist."
+	exit 1
+fi
 
+for CAT in $(cat "/${SLACKDIR}/CHECKSUMS.md5" | grep -e "\.\/${PKGDIR}" | \
+	awk '{ print $2 }' | cut -d '/' -f 3 | sort | uniq); do
+	if [ ! -d "/${SLACKDIR}/${PKGDIR}/${CAT}" ]; then
+		continue
+	fi
 ### "Old way" using Slackware's menus; not bad and fast ###
 #	sh /${SLACKDIR}/${PKGDIR}/${CAT}/maketag 2>/dev/null
 #	mkdir ${PKGDIR}/${CAT}
@@ -100,85 +107,89 @@ for CAT in `ls -gG1 /${SLACKDIR}/${PKGDIR}/ | grep ^d |\
 # dialog parameters
 	DLGPRM=''
 	TAGLIST=''
-	for LINE in `cat /${SLACKDIR}/${PKGDIR}/${CAT}/tagfile`\
-	; do
-		TAG=`echo $LINE | cut -d \: -f 1`
-		TAGLIST+="${TAG} "
-		if [ $REUSETAGS -eq 1 ]; then
-			TLINE=`cat ./${PKGDIR}.old/${CAT}/tagfile 2>/dev/null | grep ^${TAG}:`
-			if [ $? -eq 0 ]; then
-				STATE=`echo $TLINE | cut -d \: -f 2 | sed 's/ //g'`
+	for LINE in $(cat "/${SLACKDIR}/${PKGDIR}/${CAT}/tagfile"); do
+		TAG=$(echo "${LINE}" | cut -d ':' -f 1)
+		TAGLIST="${TAGLIST} ${TAG}"
+		if [ ${REUSETAGS} -eq 1 ]; then
+			TLINE=$(cat "./${PKGDIR}.old/${CAT}/tagfile" 2>/dev/null | \
+				grep -e "^${TAG}:")
+			if [ "${TLINE}" ]; then
+				STATE=$(echo "${TLINE}" | cut -d ':' -f 2 | sed -e 's/ //g')
 			else
-				STATE=`echo $LINE | cut -d \: -f 2 | sed 's/ //g'`
-			fi;
+				STATE=$(echo "${LINE}" | cut -d ':' -f 2 | sed -e 's/ //g')
+			fi
 		else
-			STATE=`echo $LINE | cut -d \: -f 2 | sed 's/ //g'`
-		fi;
+			STATE=$(echo "${LINE}" | cut -d ':' -f 2 | sed -e 's/ //g')
+		fi
 		STATETXT=''
-		case $STATE in
-			ADD)
-			OPTION='on';;
-			REC)
-			OPTION='on'
-			STATETXT='REQUIRED';;
-			SKP)
-			OPTION='off';;
+		case "${STATE}" in
+			'ADD')
+				OPTION='on'
+				;;
+			'REC')
+				OPTION='on'
+				STATETXT='REQUIRED'
+				;;
+			'SKP')
+				OPTION='off'
+				;;
 			*)
-			OPTION='off';;
+				OPTION='off'
+				;;
 		esac
-		if [ $PKGDESC -eq 1 ]; then
-#			This is broken, but I don't care. Please, send me the patch, 
-#			if you want.
-#			DESC=`head -n 1 /${SLACKDIR}/${PKGDIR}/${CAT}/${TAG}-[0-9]*.txt \
-#			2>/dev/null | cut -d \: -f 2 | sed s/[\)\(\n]//g | \
-#			sed "s/${TAG} ${TAG}//g"`
-#			TDESC=`cat /${SLACKDIR}/${PKGDIR}/${CAT}/maketag | \
-#			fgrep "\"${TAG}\"" | cut -d \" -f 4`
-#			DESC="${TDESC} ${STATE}"
-#			DLGPRM+="${TAG} \"${DESC}\" ${OPTION} "
-			DLGPRM+="${TAG} \"$STATE\" ${OPTION} "
+		if [ ${PKGDESC} -eq 1 ]; then
+			TDESC=$(grep -e "^\"${TAG}\"" "/${SLACKDIR}/${PKGDIR}/${CAT}/maketag" | \
+				cut -d '"' -f 4 | tr ' ' '_')
+			if [ -z "${TDESC}" ]; then
+				TDESC=$(head -n 1 /${SLACKDIR}/${PKGDIR}/${CAT}/${TAG}-[0-9]*.txt \
+					2>/dev/null | cut -d '(' -f 2 | sed -e 's/[()]+//g' | tr ' ' '_')
+			fi
+			if [ -z "${TDESC}" ]; then
+				TDESC="${STATE}"
+			fi
+			DLGPRM+="${TAG} ${TDESC} ${OPTION} "
 		else
 			DLGPRM+="${TAG} \"${STATE}\" ${OPTION} "
-		fi;
-	done;
-	LINES=`cat /${SLACKDIR}/${PKGDIR}/${CAT}/tagfile | wc -l`
+		fi
+	done
+	LINES=$(cat /${SLACKDIR}/${PKGDIR}/${CAT}/tagfile | wc -l)
 	cat /dev/null > ./pkgListTmp
 
+	mkdir "./${PKGDIR}/${CAT}"
+	cat /dev/null > "./${PKGDIR}/${CAT}/tagfile"
 	dialog --title "SELECTING PACKAGES FROM SERIES ${CAT}" \
        --checklist "Please confirm the packages you wish to install \
 from series ${CAT}.  Use the UP/DOWN keys to scroll through the list, and \
 the SPACE key to deselect any items you don't want to install.  \
 Press ENTER when you are done." \
-22 70 10 \
-$DLGPRM \
-2>pkgListTmp
-	
-	mkdir ./${PKGDIR}/${CAT}
-	cat /dev/null > ./${PKGDIR}/${CAT}/tagfile
+21 76 10 \
+${DLGPRM} \
+2> ./pkgListTmp
 	if [ $? = 1 -o $? = 255 ]; then
 		cat /dev/null > ./pkgListTmp
-		> $TMP/SeTnewtag
-		for TAG in $TAGLIST; do
-			echo "$TAG: SKP" >> ./${PKGDIR}/${CAT}/tagfile
-		done;
-		echo "# ${CAT} / $LINES / 0 / $LINES" >> ./${PKGDIR}/tagfiles.nfo
+#		cat /dev/null > "${TMP}/SeTnewtag"
+		for TAG in $(echo "${TAGLIST}"); do
+			echo "${TAG}: SKP" >> "./${PKGDIR}/${CAT}/tagfile"
+		done
+		echo "# ${CAT} / ${LINES} / 0 / ${LINES}" >> "./${PKGDIR}/tagfiles.nfo"
 		continue
-	fi;
+	fi
 
 	ADDED=0
 	SKIPPED=0
-	for TAG in $TAGLIST; do
-		if fgrep \"$TAG\" ./pkgListTmp 1> /dev/null 2> /dev/null ; then
-			echo "$TAG: ADD" >> ./${PKGDIR}/${CAT}/tagfile
-			ADDED=`expr $ADDED + 1`
+	for TAG in $(echo "${TAGLIST}"); do
+		if $(grep -q -e "${TAG}" ./pkgListTmp 1> /dev/null 2> /dev/null) ; then
+			echo "${TAG}: ADD" >> "./${PKGDIR}/${CAT}/tagfile"
+			ADDED=$(($ADDED+1))
 		else
-			echo "$TAG: SKP" >> ./${PKGDIR}/${CAT}/tagfile
-			SKIPPED=`expr $SKIPPED + 1`
-		fi;
-	done;
-	echo "# ${CAT} / $LINES / $ADDED / $SKIPPED" >> ./${PKGDIR}/tagfiles.nfo
+			echo "$TAG: SKP" >> "./${PKGDIR}/${CAT}/tagfile"
+			SKIPPED=$(($SKIPPED+1))
+		fi
+	done
+	echo "# ${CAT} / ${LINES} / ${ADDED} / ${SKIPPED}" \
+		>> "./${PKGDIR}/tagfiles.nfo"
 	cat /dev/null > ./pkgListTmp
-done;
+done
 rm -f ./pkgListTmp
 
 echo "Success"
