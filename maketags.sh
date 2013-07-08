b641afdea17c4bd90aa96623269f6eb6bb8e2af0
Zdenek Styblik <stybla@turnovfree.net>
2013-07-08 15:35:00 +0200

    MOD Complete overhaul of the whole thing
    Commit is a complete overhaul of the script and its features.
    Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>

--- maketags.sh
+++ maketags.sh
@@ -1,171 +1,271 @@
 #!/bin/sh
-# 09/Oct/17 @ Zdenek Styblik
-# 
+# Desc: Slackware/Slack-Kickstart Tag-file generator
+# 2013/May/09 @ Zdenek Styblik
 # "Yo Adrian, I did it!"
-#
-# Licence:
-# Everything is about licence these days. Well, since there are some 
-# parts and ideas from other scripts, and these scripts have GPLv2 
-# or better, GPLv2+ it is.
-# As usual, I don't care, but original authors resp. authors of 
-# chunks of the code might- You get the point.
-#
-# Ideas: 
-# - there could be menu A-Y like during an installation
-#
+set -e
 set -u
-# Where can we find mounted Slackware CD/DVD/etc.
-SLACKDIR='/mnt/cdrom/'
-# Read package description from TXT files
-PKGDESC=1
 
+# PATH to CHECKSUMS.md5
+SLACKDIR=${SLACKDIR:-''}
+# Output dir where tag-lists will be written
+OUTDIR=${OUTDIR:-''}
+IFS='
+'
+REUSE=0
 
-rm -f ./SLACKDIR 2>/dev/null
-rm -f ./pkgListTmp 2>/dev/null
+function print_keys_help()
+{
+	printf "Controls:\n"
+	printf "* 'a' - add package(default)\n"
+	printf "* 'd' - show description\n"
+	printf "* 'h' - print this help\n"
+	printf "* 's' - skip package\n"
+	printf "* 'v' - lock package version\n"
+	printf "* 'jj' - add all packages from current serie\n"
+	printf "* 'kk' - skip all packages from current serie\n"
+	printf "* 'zz' - skip/end selection of current serie\n"
+}
 
-# new version with dialog
-dialog --title "Slackware directory" --inputbox "Please, enter path \
-to Slackware directory \
-eg. mounted CD/DVD-ROM, or use default (${SLACKDIR})" 8 70 2>SLACKDIR
-if [ -s './SLACKDIR' ]; then
-	SLACKDIR=$(cat ./SLACKDIR)
-	rm -f ./SLACKDIR
-fi
-
-if [ ! -d "${SLACKDIR}/slackware" ] && [ ! -d "${SLACKDIR}/slackware64" ]; then 
-	echo "There seems to be no slackware directory in ${SLACKDIR}"
-	echo "Make sure Slackware is there, or check the permissions"
+if which less >/dev/null 2>&1 ; then
+	PAGER=$(which less)
+elif which more >/dev/null 2>&1 ; then
+	PAGER=$(which more)
+else
+	printf "No suitable PAGER found.\n" 1>&2
 	exit 1
 fi
 
-LSOUT=$(ls "${SLACKDIR}" 2>/dev/null | grep -E -e '^slackware(64)?$')
-# there might be more than one dir, but it's unlikely.
-PKGDIR=$(echo ${LSOUT} | cut -d ' ' -f 1)
 
-REUSETAGS=0
-if [ -d "./${PKGDIR}" ]; then
-	dialog --title "Old tag files" --yesno "It looks I've found the \
-last set of tag files you've generated. Should I re-use them, if \
-possible, or start over (= delete)?" 10 72
-	if [ $? == 0 ]; then
-		REUSETAGS=1
-		rm -Rf "./${PKGDIR}.old"
-		mv "./${PKGDIR}" "./${PKGDIR}.old"
-	else
-		REUSETAGS=0
-		rm -Rf "./${PKGDIR}"
+if [ -z "${SLACKDIR}" ]; then
+	SLACK_CD_FILE="/mnt/cdrom/"
+	printf "Please enter path to mounted Slackware CD/DVD-ROM, or directory \n"
+	printf "with 'CHECKSUMS.md5'. Empty value means to use default \n"
+	printf "directory: '%s'.\n" "${SLACKDIR}"
+	printf "Path to Slackware CD: "
+	MYTMP=""
+	read MYTMP
+	if [ ! -z "${MYTMP}" ]; then
+		SLACKDIR="${MYTMP}"
 	fi
 fi
 
-if ! $(mkdir "./${PKGDIR}" 2>/dev/null) ; then
-	echo "Unable to create directory './${PKGDIR}'"
+CHECKSUMS="${SLACKDIR}/CHECKSUMS.md5"
+if [ ! -e "${CHECKSUMS}" ]; then
+	printf "Error: '%s' doesn't exist or not readable.\n" \
+		"${CHECKSUMS}" 1>&2
 	exit 1
 fi
 
-SLACKVER=$(head "/${SLACKDIR}/README.TXT" | grep -e 'Welcome' | \
-	sed -e 's/Welcome to //' | sed -e 's/!//')
-DATE=$(date)
-cat /dev/null > "./${PKGDIR}/tagfiles.nfo"
-echo "# Slackware PKG TAG file" >> "./${PKGDIR}/tagfiles.nfo"
-echo "# Generated on ${DATE}" >> "./${PKGDIR}/tagfiles.nfo"
-echo "# for " >> "./${PKGDIR}/tagfiles.nfo"
-echo "# ${SLACKVER}" >> "./${PKGDIR}/tagfiles.nfo"
-
-if [ ! -e "/${SLACKDIR}/CHECKSUMS.md5" ]; then
-	echo "File '${SLACKDIR}/CHECKSUMS.md5' doesn't seem to exist."
-	exit 1
+SLACKVER=$(grep -e 'ANNOUNCE' "${CHECKSUMS}" | \
+	awk '{ print $2 }' | sed 's@./@@' | awk -F'.' '{ print $2 }')
+if [ -z "${SLACKVER}" ]; then
+	SLACKVER="unknown"
 fi
 
-for CAT in $(cat "/${SLACKDIR}/CHECKSUMS.md5" | grep -e "\.\/${PKGDIR}" | \
-	awk '{ print $2 }' | cut -d '/' -f 3 | sort | uniq); do
-	if [ ! -d "/${SLACKDIR}/${PKGDIR}/${CAT}" ]; then
-		continue
+SUFFIX=""
+if grep -q -e './slackware64/' "${CHECKSUMS}" ; then
+	SUFFIX="64"
+fi
+
+if [ -z "${OUTDIR}" ] && [ $# -gt 0 ]; then
+	OUTDIR=${1:-''}
+fi
+if [ -z "${OUTDIR}" ]; then
+	OUTDIR="slackware${SUFFIX}-${SLACKVER}"
+	printf "Provide a name of directory where to save tag-file structure\n"
+	printf "or empty value to use default('%s'): " "${OUTDIR}"
+	MYTMP=""
+	read MYTMP
+	if [ ! -z "${MYTMP}" ]; then
+		OUTDIR="${MYTMP}"
 	fi
-# dialog parameters
-	DLGPRM=''
-	TAGLIST=''
-	for LINE in $(cat "/${SLACKDIR}/${PKGDIR}/${CAT}/tagfile"); do
-		TAG=$(echo "${LINE}" | cut -d ':' -f 1)
-		TAGLIST="${TAGLIST} ${TAG}"
-		if [ ${REUSETAGS} -eq 1 ]; then
-			TLINE=$(cat "./${PKGDIR}.old/${CAT}/tagfile" 2>/dev/null | \
-				grep -e "^${TAG}:")
-			if [ "${TLINE}" ]; then
-				STATE=$(echo "${TLINE}" | cut -d ':' -f 2 | sed -e 's/ //g')
-			else
-				STATE=$(echo "${LINE}" | cut -d ':' -f 2 | sed -e 's/ //g')
-			fi
-		else
-			STATE=$(echo "${LINE}" | cut -d ':' -f 2 | sed -e 's/ //g')
-		fi
-		STATETXT=''
-		case "${STATE}" in
-			'ADD')
-				OPTION='on'
+fi
+
+OUTDIR_TMP=$(basename "${OUTDIR}")
+if [ "${OUTDIR_TMP}" = "." ] || [ "${OUTDIR_TMP}" = "" ]; then
+	printf "Invalid output directory given.\n" 1>&2
+	exit 1
+fi
+
+if [ -d "${OUTDIR}" ]; then
+	CDATE=$(date "+%F_%H%M%S")
+	printf "Directory '%s' already exists. Possible actions:\n" "${OUTDIR}"
+	printf "* [D]elete it\n"
+	printf "* [R]ename it to '%s-%s'\n" "${OUTDIR}" "${CDATE}"
+	printf "* re[U]se old tags\n"
+	while [ 1 -gt 0 ]; do
+		printf "What should I do?: "
+		MYTMP=""
+		read MYTMP
+		case "${MYTMP}" in
+			"d"|"D")
+				printf "Removing '%s'.\n" "${OUTDIR}"
+				rm -rf "${OUTDIR}"
+				mkdir "${OUTDIR}"
+				break
 				;;
-			'REC')
-				OPTION='on'
-				STATETXT='REQUIRED'
+			"r"|"R")
+				printf "Renaming '%s' to '%s'.\n" "${OUTDIR}" \
+					"${OUTDIR}-${CDATE}"
+				mv "${OUTDIR}" "${OUTDIR}-${CDATE}"
+				mkdir "${OUTDIR}"
+				break
 				;;
-			'SKP')
-				OPTION='off'
-				;;
-			*)
-				OPTION='off'
+			"u"|"U")
+				printf "Will re-use tag info, if possible.\n"
+				REUSE=1
+				break
 				;;
 		esac
-		if [ ${PKGDESC} -eq 1 ]; then
-			TDESC=$(grep -e "^\"${TAG}\"" "/${SLACKDIR}/${PKGDIR}/${CAT}/maketag" | \
-				cut -d '"' -f 4 | tr ' ' '_')
-			if [ -z "${TDESC}" ]; then
-				TDESC=$(head -n 1 /${SLACKDIR}/${PKGDIR}/${CAT}/${TAG}-[0-9]*.txt \
-					2>/dev/null | cut -d '(' -f 2 | sed -e 's/[()]+//g' | tr ' ' '_')
-			fi
-			if [ -z "${TDESC}" ]; then
-				TDESC="${STATE}"
-			fi
-			DLGPRM+="${TAG} ${TDESC} ${OPTION} "
-		else
-			DLGPRM+="${TAG} \"${STATE}\" ${OPTION} "
-		fi
 	done
-	LINES=$(cat /${SLACKDIR}/${PKGDIR}/${CAT}/tagfile | wc -l)
-	cat /dev/null > ./pkgListTmp
+else
+	mkdir "${OUTDIR}"
+fi
 
-	mkdir "./${PKGDIR}/${CAT}"
-	cat /dev/null > "./${PKGDIR}/${CAT}/tagfile"
-	dialog --title "SELECTING PACKAGES FROM SERIES ${CAT}" \
-       --checklist "Please confirm the packages you wish to install \
-from series ${CAT}.  Use the UP/DOWN keys to scroll through the list, and \
-the SPACE key to deselect any items you don't want to install.  \
-Press ENTER when you are done." \
-21 76 10 \
-${DLGPRM} \
-2> ./pkgListTmp
-	if [ $? = 1 -o $? = 255 ]; then
-		cat /dev/null > ./pkgListTmp
-		for TAG in ${TAGLIST}; do
-			echo "${TAG}: SKP" >> "./${PKGDIR}/${CAT}/tagfile"
+UPFILTER=""
+if [ $REUSE -ne 0 ]; then
+	printf "If you want to modify(update) only specific serie(s), then provide\n"
+	printf "space delimited list. Empty value means to modify all series: "
+	read UPFILTER
+	if [ ! -z "${UPFILTER}" ] && ! printf -- "%s" "${UPFILTER}" | \
+		grep -q -E -e '^[a-z\s]+$'; then
+		printf "Error: Invalid list of Series given.\n" 1>&2
+		exit 1
+	fi
+fi
+
+print_keys_help
+for SERIE in $(grep -E -e "./slackware${SUFFIX}/[a-z]+/" "${CHECKSUMS}" |\
+	awk '{ print $2 }' | sed 's@^./@@' | awk -F'/' '{ print $2 }' | \
+	sort | uniq); do
+	SKIP_SERIE=0
+	if [ $REUSE -ne 0 ] && [ ! -z "${UPFILTER}" ]; then
+		IFS=' '
+		FOUND=0
+		for SERTOUP in $UPFILTER; do
+			if [ -z "${SERTOUP}" ] || [ "${SERTOUP}" != ${SERIE} ]; then
+				continue
+			fi
+			FOUND=1
+			break
 		done
-		echo "# ${CAT} / ${LINES} / 0 / ${LINES}" >> "./${PKGDIR}/tagfiles.nfo"
+		IFS='
+		'
+		if [ $FOUND -eq 0 ]; then
+			continue
+		fi
+		FOUND=0
+	fi
+	if [ ! -d "${OUTDIR}/${SERIE}/" ]; then
+		mkdir "${OUTDIR}/${SERIE}/"
+	fi
+	TAGFILE="${OUTDIR}/${SERIE}/tagfile"
+	if [ $REUSE -ne 0 ] && [ -e "${TAGFILE}" ]; then
+		mv "${TAGFILE}" "${TAGFILE}.old"
+	fi
+	printf "Getting packages for serie '%s'...\n" "${SERIE}"
+	CHOICE_OVERR=""
+	for PKG in $(grep -E -e "./slackware${SUFFIX}/${SERIE}/.*\.t(g|x)z$" \
+		"${CHECKSUMS}" | awk '{ print $2 }' | sed 's@^./@@'); do
+		PKG_NOSUFF=$(printf -- "%s" "${PKG}" |\
+			awk '{ arrlen=split($0, arr, "/"); printf "%s",arr[arrlen]; }' |\
+			sed 's@.t[g|x]z$@@')
+		PKG_NAME=$(printf -- "%s" "${PKG_NOSUFF}" | \
+			awk '{ arrlen=split($0, arr, "-");
+			for (i = 1; i < (arrlen-2); i++) {
+				printf "%s", arr[i];
+				if ((i + 1) < (arrlen-2)) { printf "-" };
+			} }')
+		PKG_VER=$(printf -- "%s" "${PKG_NOSUFF}" | \
+			awk '{ arrlen=split($0, arr, "-"); printf "%s",arr[arrlen-2]; }')
+		CHOICE_DEF='a'
+		EXTRA=""
+		if [ $REUSE -ne 0 ] && [ -e "${TAGFILE}.old" ]; then
+			CHOICE_OLD=$(grep -e "${PKG_NAME}" "${TAGFILE}.old" | \
+				awk -F':' '{ print $2 }' | perl -p -e 's/\s+//')
+			if [ "${CHOICE_OLD}" = "ADD" ]; then
+				CHOICE_DEF='a'
+			elif [ "${CHOICE_OLD}" = "SKP" ]; then
+				CHOICE_DEF='s'
+			fi
+		fi
+		while [ 1 -gt 0 ]; do
+			printf "Package '%s' [%s]: %s" "${PKG_NOSUFF}" "${CHOICE_DEF}" \
+				"${CHOICE_OVERR}"
+			if [ -z "${CHOICE_OVERR}" ]; then
+				read MYCHOICE
+			else
+				MYCHOICE="${CHOICE_OVERR}"
+				printf "\n"
+			fi
+			if [ -z "${MYCHOICE}" ]; then
+				MYCHOICE="${CHOICE_DEF}"
+			fi
+			case "${MYCHOICE}" in
+				'a'|'A')
+					ACT="ADD"
+					break ;;
+				'd'|'D')
+					PKGDESC="${SLACKDIR}/slackware${SUFFIX}/${SERIE}/${PKG_NOSUFF}.txt"
+					if [ -e "${PKGDESC}" ]; then
+						$PAGER "${PKGDESC}"
+					else
+						printf "File '%s' doesn't seem to exist.\n" \
+							"${PKGDESC}" 1>&2
+					fi
+					;;
+				'h'|'H')
+					print_keys_help
+					;;
+				's'|'S')
+					ACT="SKP"
+					break
+					;;
+				'v'|'V')
+					ACT="ADD"
+					EXTRA="-${PKG_VER}"
+					break
+					;;
+				'jj'|'JJ')
+					CHOICE_OVERR='a'
+					;;
+				'kk'|'KK')
+					CHOICE_OVERR='s'
+					;;
+				'zz'|'ZZ')
+					SKIP_SERIE=1
+					break
+					;;
+			esac
+		done
+		if [ $SKIP_SERIE -ne 0 ]; then
+			break
+		fi
+		printf "%s%s: %s\n" "${PKG_NAME}"  "${EXTRA}" "${ACT}" >> "${TAGFILE}"
+	done
+	rm -f "${OUTDIR}/${SERIE}/taglist.old"
+done
+
+KSFILE=$(basename -- "${OUTDIR}")
+if [ "${KSFILE}" = "." ]; then
+	KSFILE="${OUTDIR}"
+fi
+KSFILE="${OUTDIR}/${KSFILE}.ks.tagfile"
+printf "Will create Slack-Kickstart file '%s'\n" "${KSFILE}"
+cat /dev/null > "${KSFILE}"
+printf "# Slack-Kickstart tag file\n" > "${KSFILE}"
+for SERIE in $(ls -1 "${OUTDIR}/"); do
+	if [ ! -d "${OUTDIR}/${SERIE}" ]; then
 		continue
 	fi
-
-	ADDED=0
-	SKIPPED=0
-	for TAG in ${TAGLIST}; do
-		if $(grep -q -e "${TAG}" ./pkgListTmp 1> /dev/null 2> /dev/null) ; then
-			echo "${TAG}: ADD" >> "./${PKGDIR}/${CAT}/tagfile"
-			ADDED=$(($ADDED+1))
-		else
-			echo "$TAG: SKP" >> "./${PKGDIR}/${CAT}/tagfile"
-			SKIPPED=$(($SKIPPED+1))
-		fi
-	done
-	echo "# ${CAT} / ${LINES} / ${ADDED} / ${SKIPPED}" \
-		>> "./${PKGDIR}/tagfiles.nfo"
-	cat /dev/null > ./pkgListTmp
+	if ! grep -q -e "./slackware${SUFFIX}/${SERIE}/" "${CHECKSUMS}" ; then
+		printf "Note: Serie '%s' probably became obsolete.\n" "${SERIE}" 1>&2
+	fi
+	if [ -e "${OUTDIR}/${SERIE}/tagfile" ]; then
+		printf "# Packages of serie '%s'\n" "${SERIE}" >> "${KSFILE}"
+		cat "${OUTDIR}/${SERIE}/tagfile" >> "${KSFILE}"
+	fi
 done
-rm -f ./pkgListTmp
 
-echo "Success"
+printf "All done.\n"
+# EOF
